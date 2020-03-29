bd78d28e65699167b34155aa520f999bb30885d7
Lucas Campelo <campelo.lucas@hotmail.com>
2020-03-29 18:02:13 -0300

    Realizando melhorias no mobile :iphone:

--- mobile/App.js
+++ mobile/App.js
@@ -2,11 +2,17 @@ import 'intl';
 import 'intl/locale-data/jsonp/pt-BR';
 
 import React from 'react';
+import { StatusBar, View } from 'react-native';
+import Constants from 'expo-constants';
 
 import Routes from './src/routes';
 
 export default function App() {
   return (
-    <Routes />
+    <>
+      <StatusBar barStyle="light-content" />
+      <View style={{height: Constants.statusBarHeight, backgroundColor: '#E02041'}} />
+      <Routes />
+    </>
   );
 }
